7d462c5ab2a06efb2c090a84ee978d183c6034ef
pacinidiego <pacinidiego@gmail.com>
2025-11-12 11:56:15 -0300

    Update urls.py

--- backoffice/urls.py
+++ backoffice/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
 from . import views
 
-# (S1b) Rutas para el Backoffice del Docente
+# (S1c) Rutas para el Backoffice del Docente
+app_name = 'backoffice' # Es una buena práctica para HTMX
+
 urlpatterns = [
     # La URL /backoffice/
-    path('', views.dashboard, name='backoffice_dashboard'),
+    path('', views.dashboard, name='dashboard'),
+    
+    # --- ¡NUEVO! Rutas para el Constructor de Ítems (S1c) ---
+    
+    # Esta URL entrega el formulario vacío
+    # hx-get -> /backoffice/items/create/
+    path('items/create/', views.item_create, name='item_create'),
+    
+    # Esta URL recibe los datos del formulario
+    # hx-post -> /backoffice/items/create/
+    # (Usamos la misma URL para GET y POST)
 ]
