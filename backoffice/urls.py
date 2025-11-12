c7870f7cadcbac2e376f93268079d315d5d03824
pacinidiego <pacinidiego@gmail.com>
2025-11-12 13:53:50 -0300

    Update urls.py

--- backoffice/urls.py
+++ backoffice/urls.py
@@ -2,19 +2,23 @@ from django.urls import path
 from . import views
 
 # (S1c) Rutas para el Backoffice del Docente
-app_name = 'backoffice' # Es una buena práctica para HTMX
+app_name = 'backoffice' 
 
 urlpatterns = [
-    # La URL /backoffice/
+    # Dashboard (que ahora será el formulario de subida)
     path('', views.dashboard, name='dashboard'),
     
-    # --- ¡NUEVO! Rutas para el Constructor de Ítems (S1c) ---
+    # --- ¡NUEVO! Flujo de subida de Excel (S1c) ---
     
-    # Esta URL entrega el formulario vacío
-    # hx-get -> /backoffice/items/create/
-    path('items/create/', views.item_create, name='item_create'),
+    # 1. (POST) Recibe el archivo Excel y dispara la tarea de Celery
+    path('exam/upload/', views.exam_upload_view, name='exam_upload'),
     
-    # Esta URL recibe los datos del formulario
-    # hx-post -> /backoffice/items/create/
-    # (Usamos la misma URL para GET y POST)
+    # 2. (GET) La URL que HTMX "pollea" (pregunta) para ver si la tarea terminó
+    path('exam/poll-task/<str:task_id>/', views.poll_task_status_view, name='poll_task_status'),
+    
+    # 3. (GET) La página del "Constructor" (la "versión no grabada")
+    path('exam/<int:exam_id>/constructor/', views.exam_constructor_view, name='exam_constructor'),
+    
+    # 4. (GET) La URL para descargar la plantilla modelo
+    path('download-template/', views.download_excel_template_view, name='download_template'),
 ]
