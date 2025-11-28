a5d423c9b3ddf7f51009e42153f32ae1c7b96cab
pacinidiego <pacinidiego@gmail.com>
2025-11-28 15:59:15 -0300

    Update urls.py

--- runner/urls.py
+++ runner/urls.py
@@ -4,6 +4,14 @@ from . import views
 app_name = 'runner'
 
 urlpatterns = [
+    # 1. Lobby (Entrada)
     path('<uuid:access_code>/', views.lobby_view, name='lobby'),
-    path('<uuid:access_code>/take/', views.exam_runner_view, name='take'),
+    
+    # 2. Tech Check (NUEVO: Paso intermedio)
+    # Necesitamos el attempt_id para saber quién es el que está probando la cámara
+    path('<uuid:access_code>/check/<uuid:attempt_id>/', views.tech_check_view, name='tech_check'),
+
+    # 3. Runner (El examen real)
+    # Ahora también recibe attempt_id para saber qué intento retomar
+    path('<uuid:access_code>/take/<uuid:attempt_id>/', views.exam_runner_view, name='take'),
 ]
