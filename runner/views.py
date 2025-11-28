ab3e2cd2b6bb371cf0ef1512cb615ef03198b397
pacinidiego <pacinidiego@gmail.com>
2025-11-28 15:39:01 -0300

    Update views.py

--- runner/views.py
+++ runner/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from exams.models import Exam
 
-# Create your views here.
+def lobby_view(request, access_code):
+    # Si esto falla, devuelve 404. 
+    # Pero al menos sabremos que llegó a la vista.
+    exam = get_object_or_404(Exam, access_code=access_code)
+    return HttpResponse(f"<h1>Lobby del Examen: {exam.title}</h1>")
+
+def exam_runner_view(request, access_code):
+    return HttpResponse("<h1>Runner</h1>")
