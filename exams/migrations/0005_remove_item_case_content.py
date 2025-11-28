d08cf0c1379ae6924e0614b34746b172692e1a7d
pacinidiego <pacinidiego@gmail.com>
2025-11-28 20:59:22 -0300

    Create 0005_remove_item_case_content.py

---
+++ exams/migrations/0005_remove_item_case_content.py
@@ -0,0 +1,16 @@
+# exams/migrations/0005_remove_item_case_content.py
+
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('exams', '0004_alter_exam_options_alter_item_options_and_more'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='item',
+            name='case_content',
+        ),
+    ]
