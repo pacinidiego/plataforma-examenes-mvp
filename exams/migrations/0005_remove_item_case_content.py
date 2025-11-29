3476ff909eac08258244a799deb3f303d773d879
pacinidiego <pacinidiego@gmail.com>
2025-11-28 21:10:21 -0300

    Create 0005_remove_item_case_content.py

---
+++ exams/migrations/0005_remove_item_case_content.py
@@ -0,0 +1,12 @@
+# exams/migrations/0005_remove_item_case_content.py
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('exams', '0004_alter_exam_options_alter_item_options_and_more'),
+    ]
+
+    operations = [
+        # Operación vacía intencionalmente para evitar error de columna inexistente
+    ]
