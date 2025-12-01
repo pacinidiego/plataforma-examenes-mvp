5bdf338ab39822cb06b736a23dc619c27e018460
pacinidiego <pacinidiego@gmail.com>
2025-12-01 10:17:19 -0300

    Update urls.py

--- runner/urls.py
+++ runner/urls.py
@@ -5,15 +5,22 @@ app_name = 'runner'
 
 urlpatterns = [
     # 1. Lobby (Entrada)
-    path('<uuid:access_code>/', views.lobby_view, name='lobby'),
+    # Cambié name='lobby' a 'lobby_view' para que funcione el botón "Volver" de la pantalla final
+    path('<uuid:access_code>/', views.lobby_view, name='lobby_view'),
     
-    # 2. Tech Check (NUEVO: Paso intermedio)
-    # Necesitamos el attempt_id para saber quién es el que está probando la cámara
+    # 2. Tech Check
     path('<uuid:access_code>/check/<uuid:attempt_id>/', views.tech_check_view, name='tech_check'),
 
     # 3. Runner (El examen real)
-    # Ahora también recibe attempt_id para saber qué intento retomar
-    path('<uuid:access_code>/take/<uuid:attempt_id>/', views.exam_runner_view, name='take'),
-    # NUEVA RUTA: Guardar respuesta (AJAX/Fetch)
+    path('<uuid:access_code>/take/<uuid:attempt_id>/', views.exam_runner_view, name='exam_runner'),
+
+    # 4. Guardar respuesta (AJAX/Fetch)
     path('save/<uuid:attempt_id>/', views.save_answer, name='save_answer'),
+
+    # === RUTAS NUEVAS (CRÍTICAS PARA FINALIZAR) ===
+    # Esta es la que busca el botón "Entregar"
+    path('submit/<uuid:attempt_id>/', views.submit_exam_view, name='submit_exam'),
+    
+    # Esta es la pantalla de "Gracias / Nota final"
+    path('finished/<uuid:attempt_id>/', views.exam_finished_view, name='exam_finished'),
 ]
