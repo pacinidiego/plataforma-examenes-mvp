86df7046b4593d1271d86e8a60fe7f1d20f1df33
pacinidiego <pacinidiego@gmail.com>
2025-12-01 10:30:21 -0300

    Update urls.py

--- runner/urls.py
+++ runner/urls.py
@@ -4,9 +4,8 @@ from . import views
 app_name = 'runner'
 
 urlpatterns = [
-    # 1. Lobby (Entrada)
-    # Cambié name='lobby' a 'lobby_view' para que funcione el botón "Volver" de la pantalla final
-    path('<uuid:access_code>/', views.lobby_view, name='lobby_view'),
+    # 1. Lobby (Volvemos a name='lobby' para que coincida con tus templates)
+    path('<uuid:access_code>/', views.lobby_view, name='lobby'),
     
     # 2. Tech Check
     path('<uuid:access_code>/check/<uuid:attempt_id>/', views.tech_check_view, name='tech_check'),
@@ -17,10 +16,7 @@ urlpatterns = [
     # 4. Guardar respuesta (AJAX/Fetch)
     path('save/<uuid:attempt_id>/', views.save_answer, name='save_answer'),
 
-    # === RUTAS NUEVAS (CRÍTICAS PARA FINALIZAR) ===
-    # Esta es la que busca el botón "Entregar"
+    # === RUTAS NUEVAS ===
     path('submit/<uuid:attempt_id>/', views.submit_exam_view, name='submit_exam'),
-    
-    # Esta es la pantalla de "Gracias / Nota final"
     path('finished/<uuid:attempt_id>/', views.exam_finished_view, name='exam_finished'),
 ]
