46c0783d510afc20048dc52dea899aae8b6a93ac
pacinidiego <pacinidiego@gmail.com>
2025-12-02 13:51:32 -0300

    Update 0001_initial.py

--- runner/migrations/0001_initial.py
+++ runner/migrations/0001_initial.py
@@ -1,19 +1,20 @@
 # runner/migrations/0001_initial.py
-import django.db.models.deletion
 import uuid
 from django.conf import settings
 from django.db import migrations, models
+import django.db.models.deletion
 
 class Migration(migrations.Migration):
 
     initial = True
 
     dependencies = [
-        ('exams', '0005_remove_item_case_content'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('exams', '0006_remove_item_case_content'),
     ]
 
     operations = [
+        # 1. Tabla de Intentos (El Examen)
         migrations.CreateModel(
             name='Attempt',
             fields=[
@@ -22,7 +23,7 @@ class Migration(migrations.Migration):
                 ('student_legajo', models.CharField(blank=True, max_length=100, verbose_name='Legajo/DNI')),
                 ('start_time', models.DateTimeField(auto_now_add=True, verbose_name='Inicio')),
                 ('end_time', models.DateTimeField(blank=True, null=True, verbose_name='Fin')),
-                ('completed_at', models.DateTimeField(blank=True, null=True, verbose_name='Completado el')), # CAMPO CLAVE
+                ('completed_at', models.DateTimeField(blank=True, null=True, verbose_name='Completado el')),
                 ('last_heartbeat', models.DateTimeField(auto_now=True)),
                 ('ip_address', models.GenericIPAddressField(blank=True, null=True)),
                 ('answers', models.JSONField(blank=True, default=dict)),
@@ -33,6 +34,21 @@ class Migration(migrations.Migration):
             ],
             options={
                 'ordering': ['-start_time'],
+            },
+        ),
+        # 2. Tabla de Eventos (La Seguridad / Caja Negra)
+        migrations.CreateModel(
+            name='AttemptEvent',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('timestamp', models.DateTimeField(auto_now_add=True)),
+                ('event_type', models.CharField(choices=[('SESSION_RESUME', 'Reconexión / Reanudación'), ('FOCUS_LOST', 'Pérdida de Foco (Cambio de Pestaña)'), ('FULLSCREEN_EXIT', 'Salida de Pantalla Completa'), ('MULTI_FACE', 'Múltiples rostros detectados'), ('NO_FACE', 'Rostro no detectado'), ('AUDIO_SPIKE', 'Sonido/Voz detectada')], max_length=50)),
+                ('metadata', models.JSONField(blank=True, default=dict)),
+                ('evidence_url', models.URLField(blank=True, null=True)),
+                ('attempt', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='events', to='runner.attempt')),
+            ],
+            options={
+                'ordering': ['timestamp'],
             },
         ),
     ]
