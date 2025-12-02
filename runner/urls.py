8ac4ccb9b8127e967b2b8769d1b2de6d4a245407
pacinidiego <pacinidiego@gmail.com>
2025-12-02 14:09:33 -0300

    Update urls.py

--- runner/urls.py
+++ runner/urls.py
@@ -5,21 +5,19 @@ app_name = 'runner'
 
 urlpatterns = [
     # 1. Lobby
-    # Nombre: 'lobby' (como espera tu template lobby.html)
     path('<uuid:access_code>/', views.lobby_view, name='lobby'),
     
     # 2. Tech Check
-    # Nombre: 'tech_check'
     path('<uuid:access_code>/check/<uuid:attempt_id>/', views.tech_check_view, name='tech_check'),
 
-    # 3. Runner (El examen)
-    # Nombre: 'take' (CRÍTICO: así lo llama tu template tech_check.html)
+    # 3. Runner
     path('<uuid:access_code>/take/<uuid:attempt_id>/', views.exam_runner_view, name='take'),
 
-    # 4. Guardar respuesta (AJAX)
+    # 4. AJAX / Funcionalidad
     path('save/<uuid:attempt_id>/', views.save_answer, name='save_answer'),
-
-    # 5. Finalizar (Nuevas rutas)
     path('submit/<uuid:attempt_id>/', views.submit_exam_view, name='submit_exam'),
     path('finished/<uuid:attempt_id>/', views.exam_finished_view, name='exam_finished'),
+    
+    # 5. Seguridad (NUEVO)
+    path('log/<uuid:attempt_id>/', views.log_event, name='log_event'),
 ]
