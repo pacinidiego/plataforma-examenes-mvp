faaaf363dc81ff664aba03afd066e7a71811080d
pacinidiego <pacinidiego@gmail.com>
2025-12-10 16:13:13 -0300

    Update urls.py

--- classroom_exams/urls.py
+++ classroom_exams/urls.py
@@ -5,7 +5,8 @@ app_name = 'classroom_exams'
 
 urlpatterns = [
     path('', views.acceso_alumno, name='acceso'),
-    path('rendir/', views.rendir_examen, name='rendir_examen'),
+    path('reglas/', views.instrucciones_examen, name='instrucciones'), # <--- NUEVA
+    path('rendir/', views.rendir_examen, name='rendir_examen'), # Quitamos el número de la URL para que no hagan trampa escribiendo /rendir/5
     path('resultado/', views.resultado_examen, name='resultado_examen'),
     path('accion-profesor/', views.accion_profesor, name='accion_profesor'),
 ]
